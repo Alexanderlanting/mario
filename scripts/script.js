bffd4b8f0801bf0bfa22752a901512088480c9e9
Alexanderlanting <90254397+Alexanderlanting@users.noreply.github.com>
2023-04-24 09:03:05 +0200

    Delete scripts directory

--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-const background = document.querySelector('.background');
-
-window.addEventListener('scroll', () => {
-  const xPos = -window.scrollY / 2;
-  background.style.transform = `translateX(${xPos}px)`;
-});
